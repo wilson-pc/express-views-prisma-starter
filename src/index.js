f2469f2efa94780d0164aa6fde2157a70f8bc85f
wilson-pc <wilverhidalgo@outlook.com>
2022-11-25 16:51:14 -0400

    update docker

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import app from './app';
 
 require('dotenv').config();
 
+const hostname = '0.0.0.0';
+
 const port = process.env.PORT || 3005;
 
-app.listen(port, () => {
-  console.log(`Ejemplo app listening at http://localhost:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
