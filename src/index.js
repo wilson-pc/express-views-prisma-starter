f2eac0aab8c7cd7227c0c2224b4a5053f97ac356
wilson-pc <wilverhidalgo@outlook.com>
2022-11-25 12:32:36 -0400

    renma e

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app';
 
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3005;
 
 app.listen(port, () => {
   console.log(`Ejemplo app listening at http://localhost:${port}`);
